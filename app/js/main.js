86256956a3a37dd08d374b5a018e8bb02cfcaa02
warnickb <35404212+warnickb@users.noreply.github.com>
2019-04-13 16:11:41 -0400

    added cam controls, car, tree

--- app/js/main.js
+++ app/js/main.js
@@ -5,16 +5,38 @@ import Wheel from './models/Wheel';
 import TrackballControls from 'three-trackballcontrols';
 
 //car objects
-const dodecgeom = new THREE.DodecahedronGeometry(30);
-const dodecmatr = new THREE.MeshBasicMaterial({ color: 0x14ae6e });
-const dodecmesh = new THREE.Mesh(dodecgeom, dodecmatr);
 
-const carGeom = new THREE.CubeGeometry(20, 20, 20);
-const carMatr = new THREE.MeshBasicMaterial({ color: 0x14ae7e });
-const car = new THREE.Mesh(carGeom, carMatr);
 
-//singleGeometry = new THREE.Geometry();
-//singleGeometry.merge(dodecmesh.Mesh, car.Mesh);
+const landGeom = new THREE.CubeGeometry(500, 50, 400, 40);
+const landMatr = new THREE.MeshBasicMaterial({ color: 0x91ef00 });
+const land = new THREE.Mesh(landGeom, landMatr);
+land.position.z = -50;
+land.position.y = -40;
+
+const box = new THREE.BoxGeometry(20, 10, 10);
+const sphere = new THREE.SphereGeometry(10, 20, 20);
+const singleGeom = new THREE.Geometry();
+const boxMesh = new THREE.Mesh(box);
+const sphereMesh = new THREE.Mesh(sphere);
+boxMesh.updateMatrix();
+singleGeom.merge(boxMesh.geometry, boxMesh.matrix);
+sphereMesh.updateMatrix();
+singleGeom.merge(sphereMesh.geometry, sphereMesh.matrix);
+const mat = new THREE.MeshPhongMaterial({ color: 0xFFF000 });
+const newMesh = new THREE.Mesh(singleGeom, mat);
+
+const trunkGeom = new THREE.CylinderGeometry(8, 8, 10, 10);
+const trunkMatr = new THREE.MeshBasicMaterial({ color: 0x808000 });
+const trunk = new THREE.Mesh(trunkGeom, trunkMatr);
+trunk.position.set(10, -5, 15);
+
+const leafGeom = new THREE.ConeGeometry(15, 35, 15, 15);
+const leafMatr = new THREE.MeshBasicMaterial({ color: 0x008000});
+const leaf = new THREE.Mesh(leafGeom, leafMatr);
+leaf.position.set(10, 13, 15);
+
+const camera = new THREE.PerspectiveCamera(75, 4 / 3, 0.5, 500);
+
 
 export default class App {
     constructor() {
@@ -26,24 +48,29 @@ export default class App {
         //   Field of view: 75 degrees
         //   Screen aspect ration 4:3
         //   Near plane at z=0.5, far plane at z=500
-        this.camera = new THREE.PerspectiveCamera(75, 4 / 3, 0.5, 500);
+        //camera = new THREE.PerspectiveCamera(75, 4 / 3, 0.5, 500);
         // Place the camera at (0,0,100)
-        this.camera.position.z = 100;
+        camera.position.z = 100;
 
         // const orbiter = new OrbitControls(this.camera);
         // orbiter.enableZoom = false;
         // orbiter.update();
-        this.tracker = new TrackballControls(this.camera);
+        this.tracker = new TrackballControls(camera);
         this.tracker.rotateSpeed = 2.0;
         // Allow zoom and pan
         this.tracker.noZoom = false;
         this.tracker.noPan = false;
 
-        // Dodecahedron radius = 30
+        
 
         //this.scene.add(dodecmesh);
 
-        this.scene.add(car);
+        this.scene.add(land);
+        this.scene.add(newMesh);
+        newMesh.add(camera);
+
+        this.scene.add(trunk);
+        this.scene.add(leaf);
 
         window.addEventListener('keydown', () => this.onDocumentKeyDown(event));
         
@@ -54,10 +81,14 @@ export default class App {
 
     }
     render() {
-        this.renderer.render(this.scene, this.camera);
+        this.renderer.render(this.scene, camera);
         this.tracker.update();
         // setup the render function to "autoloop"
         requestAnimationFrame(() => this.render());
+
+        camera.lookAt(newMesh.position);
+        camera.updateProjectionMatrix();
+        //camera.lookAt(car.position.y);
     }
 
     resizeHandler() {
@@ -70,22 +101,22 @@ export default class App {
         }
         canvas.width = w;
         canvas.height = h;
-        this.camera.updateProjectionMatrix();
+        camera.updateProjectionMatrix();
         this.renderer.setSize(w, h);
         this.tracker.handleResize();
     }
     onDocumentKeyDown(event) {
         if (event.keyCode == 65 || event.keyCode == 37) {
-            car.position.x -= 1.0;
+            newMesh.position.x -= 5.0;
         }
         else if (event.keyCode == 68 || event.keyCode == 39) {
-            car.position.x += 1.0;
+            newMesh.position.x += 5.0;
         }
         else if (event.keyCode == 87 || event.keyCode == 38) {
-            car.position.y += 1.0;
+            newMesh.position.z -= 5.0;
         }
         else if (event.keyCode == 83 || event.keyCode == 40) {
-            car.position.y -= 1.0;
+            newMesh.position.z += 5.0;
         }
         else {
 
